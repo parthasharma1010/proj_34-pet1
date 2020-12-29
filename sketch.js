14170c926140e61e57d9dce6c6ca6afd87973b6c
parthasharma1010 <70380779+parthasharma1010@users.noreply.github.com>
2020-12-29 14:27:12 +0530

    ok
    ok

---
+++ sketch.js
@@ -0,0 +1,61 @@
+//Create variables here
+var dog, dogImg, happyDogImg, database, foodS, foodStock;
+
+function preload() {
+  dogImg = loadImage("dogImg.png");
+  happyDogImg = loadImage("dogImg1.png");
+}
+
+function setup() {
+	createCanvas(500, 500);
+  database = firebase.database();
+  foodStock = database.ref("Food");
+  foodStock.on("value",readStock);
+  foodStock.set(20);
+
+  dog = createSprite(250,350,10,60);
+  dog = addImage(dogImg);
+  dog.scale = 0.2;
+}
+
+function draw() {  
+  background(46,139,87);
+  if(foodS == undefined) {
+    textSize(20);
+    fill(255);
+    text("Note: Press Up_Arrow to feed Drago Milk",50,50);
+    text("Food Remaining :",foodS,150,150);
+
+    if(keyWentDown(UP_ARROW)) {
+      writeStock(foodS);
+      dog.addImage(happyDogImg);
+    }
+
+    if(keyWentUp(UP_ARROW)) {
+      dog.addImage(dogImg);
+    }
+
+    if(foodS === 0 ) {
+      foodS = 20;
+    }
+
+  drawSprites();
+  //add styles here
+  }
+}
+function readStock(data) {
+  foodS = data.val();
+}
+
+function writeStock(x) {
+  if(x<=0) {
+    x = 0;
+  }
+  else {
+    x = x-1;
+  }
+  database.ref("/").update({
+    Food:x
+  })
+}
+  
